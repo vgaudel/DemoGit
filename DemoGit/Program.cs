230bc1d2434628c9bb6d4478b88ef39adaf1196a
vgaudel <vgaudel@gmail.com>
2024-10-11 15:45:52 +0200

    modif message 3eme

--- DemoGit/Program.cs
+++ DemoGit/Program.cs
@@ -7,6 +7,7 @@ namespace DemoGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Universe!");
         }
     }
 }
